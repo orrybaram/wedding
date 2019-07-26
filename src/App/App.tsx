12ae9bab507b9f977bb51ed62f26d38f0cb0b381
orrybaram <orrybaram@gmail.com>
2019-07-26 01:40:03 -0400

    feat(ZolaRegistry): add registry

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,12 +1,17 @@
-import * as React from 'react';
+import React from 'react';
 import Header from './components/Header';
 import './app.styles.css';
 import Home from '../Screens/Home';
+import ZolaRegistry from '../components/ZolaRegistry';
 
 const App = () => (
   <>
     <Header />
     <Home />
+    <ZolaRegistry
+      url="www.zola.com/registry/hannaandorry"
+      registryKey="hannaandorry"
+    />
   </>
 );
 
